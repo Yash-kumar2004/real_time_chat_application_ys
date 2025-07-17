c3920db5a1774f7e6a949031ab48bc5d0fca099d
Yash Saxena <yashsaxena933@gmail.com>
2025-07-17 16:33:32 +0530

    Sign Up succusfull

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "../src/routes/message.route.js"
 import dotenv from "dotenv"
 import connectDB from "./lib/db.js"
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 dotenv.config({})
 const app=express()
 
@@ -14,6 +15,10 @@ const PORT=process.env.PORT || 3000
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 connectDB()
 app.listen(PORT,()=>{
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { protectroute } from "../middlewares/auth.middleware.js"; 
-import {getUsersForSideBar} from "../controllers/message.controller.js"
+import {getUsersForSideBar,getmessages,sendMessage} from "../controllers/message.controller.js"
 const router=Router()
 
 router.route("/users").get(protectroute,getUsersForSideBar)
+router.route("/:id").get(protectroute,getmessages)
+router.route("/send/:id").post(protectroute,sendMessage)
+
+
+export  default router
